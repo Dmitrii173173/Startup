77c03f4572710a61bddb4db6b79888dcb827b5ff
dmitrii <perfilev.dmitrii2020@gmail.com>
2024-08-10 15:36:04 +0800

    first commit

---
+++ src/cfehome/views.py
@@ -0,0 +1,21 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_template = "home.html"
+    return render(request, html_template, my_context)
+
+def about_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_template = "about.html"
+    return render(request, html_template, my_context)
